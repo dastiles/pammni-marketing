51d7a5c1064f019ec0ba8e23f962cc8d9009d64e
stiles23 <charles@codevirtus.com>
2024-01-30 12:41:23 +0200

    confirm database

--- src/utils/appwrite.jsx
+++ src/utils/appwrite.jsx
@@ -1,13 +1,22 @@
-import { Client, Databases, Account, ID } from "appwrite";
+import { Client, Databases, Account } from "appwrite";
 
 const client = new Client();
-const id = new ID();
+
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("65b7a2409b3fbb90afe8");
 
 const account = new Account(client);
 
+account.createEmailSession("dastilesforever@gmail.com", "ch@rlesm@dhuku").then(
+  function (response) {
+    console.log(response); // Success
+  },
+  function (error) {
+    console.log(error); // Failure
+  }
+);
+
 const appwriteDatabase = new Databases(client, "65b7a264bdbc83edc4e1");
 
-export { client, appwriteDatabase, account, id };
+export { client, appwriteDatabase };
